8bed625d548ab400cb41c5618f3a3ab37ec83947
ssd71 <shibanidash71@gmail.com>
2022-03-01 22:22:06 +0530

    add primitive parsing functionality

--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -44,9 +44,12 @@ public class Lox {
     Scanner scanner = new Scanner(source);
     List<Token> tokens = scanner.scanTokens();
 
-    for (Token token: tokens) {
-      System.out.println(token);
-    }
+    Parser parser = new Parser(tokens);
+    Expr expr = parser.parse();
+
+    if (hadError) return;
+
+    System.out.println(new AstPrinter().print(expr));
   }
 
   static void error(int line, String message) {
@@ -58,5 +61,13 @@ public class Lox {
       "[line " + line + "] Error" + where + ": " + message
     );
     hadError = true;
+  }
+
+  static void error(Token token, String message) {
+    if (token.type == TokenType.EOF) {
+      report(token.line, " at end", message);
+    } else {
+      report(token.line, " at '" + token.lexeme + "'", message);
+    }
   }
 }
